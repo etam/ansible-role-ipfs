8b5c23069c2bad250ea2ba93c97539dfe8e65d0c
Adam Mizerski <adam@mizerski.pl>
2021-12-29 13:24:02 +0100

    test: cleanup vagrant scripts

--- tests/vagrant_ssh_config_update.sh
+++ tests/vagrant_ssh_config_update.sh
@@ -8,10 +8,9 @@ name="$2"
 case "$action" in
     add)
         vagrant ssh-config "$name" > ".vagrant-ssh-config-${name}"
-        ssh -F ".vagrant-ssh-config-${name}" "$name" cat .ssh/authorized_keys > ".vagrant-authorized_keys-${name}"
         ;;
     remove)
-        rm -f ".vagrant-ssh-config-${name}" ".vagrant-authorized_keys-${name}"
+        rm -f ".vagrant-ssh-config-${name}"
         ;;
     *)
         echo "unknown action" >&2
@@ -23,11 +22,4 @@ if (( "${#configs[@]}" > 0 )); then
      cat "${configs[@]}" > .vagrant-ssh-config
 else
      rm -f .vagrant-ssh-config
-fi
-
-authorized_keys=(.vagrant-authorized_keys-*)
-if (( "${#authorized_keys[@]}" > 0 )); then
-    cat "${authorized_keys[@]}" > authorized_keys
-else
-    rm -f authorized_keys
 fi
